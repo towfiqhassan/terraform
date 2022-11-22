5140289f4a957a729cc0ee2835ebbaf7f702619c
towfiqhassan <33076730+towfiqhassan@users.noreply.github.com>
2022-11-22 15:03:18 +1100

    Add files via upload

---
+++ example-two-tier-application/main.tf
@@ -0,0 +1,317 @@
+#https://levelup.gitconnected.com/terraform-two-tier-architecture-66fa07c8e325
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+}
+
+#Below are region/az specific variables
+# A. grab the ami from aws console
+# B. create KeyPair upfront in the chosen region
+
+locals {
+  ami_name = "ami-089a545a9ed9893b6"
+  key_pair_name = "global-dev-th-keyPair-region-ohio"
+}
+
+# Configure provider
+provider "aws" {
+  region  = "us-east-2"
+}
+
+# Create VPC
+resource "aws_vpc" "vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name        = "vpc-project"
+  }
+}
+
+# Create internet gateway
+resource "aws_internet_gateway" "ig" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name        = "ig-project"
+  }
+}
+
+# Create 2 public subnets
+resource "aws_subnet" "public_1" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-2a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-1"
+  }
+}
+
+resource "aws_subnet" "public_2" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-2b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-2"
+  }
+}
+
+# Create 2 private subnets
+resource "aws_subnet" "private_1" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.0.3.0/24"
+  availability_zone = "us-east-2a"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "private-1"
+  }
+}
+
+resource "aws_subnet" "private_2" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.0.4.0/24"
+  availability_zone = "us-east-2b"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "private-2"
+  }
+}
+
+# Create route table to internet gateway
+resource "aws_route_table" "project_rt" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.ig.id
+  }
+  tags = {
+    Name = "project-rt"
+  }
+}
+
+# Associate public subnets with route table
+resource "aws_route_table_association" "public_route_1" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.project_rt.id
+}
+
+resource "aws_route_table_association" "public_route_2" {
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.project_rt.id
+}
+
+# Create security groups
+resource "aws_security_group" "public_sg" {
+  name        = "public-sg"
+  description = "Allow web and ssh traffic"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port         = 22
+    to_port           = 22
+    protocol          = "tcp"
+    cidr_blocks       = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+}
+
+resource "aws_security_group" "private_sg" {
+  name        = "private-sg"
+  description = "Allow web tier and ssh traffic"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    from_port        = 3306
+    to_port          = 3306
+    protocol         = "tcp"
+    cidr_blocks      = ["10.0.0.0/16"]
+    security_groups = [ aws_security_group.public_sg.id ]
+  }
+  ingress {
+    from_port         = 22
+    to_port           = 22
+    protocol          = "tcp"
+    cidr_blocks       = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+}
+
+# Security group for ALB
+resource "aws_security_group" "alb_sg" {
+  name        = "alb-sg"
+  description = "security group for alb"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    from_port   = "0"
+    to_port     = "0"
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = "0"
+    to_port     = "0"
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Create ALB
+resource "aws_lb" "project_alb" {
+  name               = "alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = [aws_subnet.public_1.id, aws_subnet.public_2.id]
+}
+
+# Create ALB target group
+resource "aws_lb_target_group" "project_tg" {
+  name     = "project-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.vpc.id
+
+  depends_on = [aws_vpc.vpc]
+}
+
+# Create target attachments
+resource "aws_lb_target_group_attachment" "tg_attach1" {
+  target_group_arn = aws_lb_target_group.project_tg.arn
+  target_id        = aws_instance.web1.id
+  port             = 80
+
+  depends_on = [aws_instance.web1]
+}
+
+resource "aws_lb_target_group_attachment" "tg_attach2" {
+  target_group_arn = aws_lb_target_group.project_tg.arn
+  target_id        = aws_instance.web2.id
+  port             = 80
+
+  depends_on = [aws_instance.web2]
+}
+
+# Create listener
+resource "aws_lb_listener" "listener_lb" {
+  load_balancer_arn = aws_lb.project_alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.project_tg.arn
+  }
+}
+
+# Create ec2 instances
+resource "aws_instance" "web1" {
+  ami           = local.ami_name
+  instance_type = "t2.micro"
+  key_name          = local.key_pair_name
+  availability_zone = "us-east-2a"
+  vpc_security_group_ids      = [aws_security_group.public_sg.id]
+  subnet_id                   = aws_subnet.public_1.id
+  associate_public_ip_address = true
+  user_data = <<-EOF
+        #!/bin/bash
+        yum update -y
+        yum install httpd -y
+        systemctl start httpd
+        systemctl enable httpd
+        echo "<html><body><h1>Hi there from us-east-2a</h1></body></html>" > /var/www/html/index.html
+        EOF
+
+  tags = {
+    Name = "web1_instance"
+  }
+}
+resource "aws_instance" "web2" {
+  ami           = local.ami_name
+  instance_type = "t2.micro"
+  key_name          = local.key_pair_name
+  availability_zone = "us-east-2b"
+  vpc_security_group_ids      = [aws_security_group.public_sg.id]
+  subnet_id                   = aws_subnet.public_2.id
+  associate_public_ip_address = true
+  user_data = <<-EOF
+        #!/bin/bash
+        yum update -y
+        yum install httpd -y
+        systemctl start httpd
+        systemctl enable httpd
+        echo "<html><body><h1>Hi there from us-east-2b</h1></body></html>" > /var/www/html/index.html
+        EOF
+
+  tags = {
+    Name = "web2_instance"
+  }
+}
+
+# Database subnet group
+resource "aws_db_subnet_group" "db_subnet"  {
+  name       = "db-subnet"
+  subnet_ids = [aws_subnet.private_1.id, aws_subnet.private_2.id]
+}
+
+# Create database instance
+resource "aws_db_instance" "project_db" {
+  allocated_storage    = 5
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  identifier           = "db-instance"
+  db_name              = "project_db"
+  username             = "admin"
+  password             = "password"
+  db_subnet_group_name = aws_db_subnet_group.db_subnet.id
+  vpc_security_group_ids = [aws_security_group.private_sg.id]
+  publicly_accessible = false
+  skip_final_snapshot  = true
+}
+
+# Outputs
+# Ec2 instance public ipv4 address
+output "ec2_public_ip" {
+  value = aws_instance.web1.public_ip
+}
+
+# Db instance address
+output "db_instance_address" {
+  value = aws_db_instance.project_db.address
+}
+
+# Getting the DNS of load balancer
+output "lb_dns_name" {
+  description = "The DNS name of the load balancer"
+  value       = "${aws_lb.project_alb.dns_name}"
+}
